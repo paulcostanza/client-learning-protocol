14727a62a34782009d29a036abfa9b0cdfca454b
Paul Costanza <paulvincentcostanza@gmail.com>
2025-01-16 13:13:16 -0500

    Edited Intro page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import Main from '../components/Main'
 import KnowledgeCheck from '../components/KnowledgeCheck'
 
 export default function Home() {
