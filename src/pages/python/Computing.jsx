8c73ed6ad6b7b0c3f3e3f3242d80e65b2e0788b0
paulcostanza <paulvincentcostanza@gmail.com>
2025-02-03 22:00:14 -0500

    updated python>computing with react-syntax-highlighter

--- src/pages/python/Computing.jsx
+++ src/pages/python/Computing.jsx
@@ -1,4 +1,50 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 export default function Computing() {
+
+    const scientificNotation = `print(16e3)
+# prints 16000.0
+
+print(7.1e-2)
+# prints 0.071`
+
+    const underScores = `num = 69_000
+print(num)
+# 69000
+
+num2 = 789_012_345.67
+print(num2)
+# 789012345.67`
+
+    const binaryInPython = `print(0b0001)
+# Prints 1
+
+print(0b0101)
+# Prints 5`
+
+    const bitwiseAndOperator = `  0101
+& 0111
+------
+  0101`
+
+    const andOperator = `0 & 0 = 0
+1 & 1 = 1
+1 & 0 = 0`
+
+    const binaryNotation = `0b0101 & 0b0111
+# equals 5
+
+binary_five = 0b0101
+binary_seven = 0b0111
+binary_five & binary_seven
+# equals 5`
+
+    const convertBinary = `# this is a binary string
+binary_string = "100"
+
+# convert binary string to integer
+num = int(binary_string, 2)`
     return (
         <>
             <h1>Computing</h1>
@@ -9,36 +55,31 @@ export default function Computing() {
 
             <p>You can add the letter <code>e</code> followed by a positive or negative integer to specify that you're using scientific notation.</p>
 
-            <pre class="hljs"><code><div>print(<span class="hljs-number">16e3</span>)<br />
-                <span class="hljs-comment"># prints 16000.0</span><br /><br />
-
-                print(<span class="hljs-number">7.1e-2</span>)<br />
-                <span class="hljs-comment"># prints 0.071</span>
-            </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {scientificNotation}
+                </SyntaxHighlighter>
+            </div>
 
             <h2 id="underscores-for-readability">Underscores for Readability</h2>
 
             <p>Python also allows you to represent large numbers in the decimal format using underscores as the delimiter instead of commas to make it easier to read.</p>
 
-            <pre class="hljs"><code><div>num = <span class="hljs-number">69</span>_000
-                print(num)<br />
-                <span class="hljs-comment"># 69000</span><br /><br />
-
-                num2 = <span class="hljs-number">789</span>_012_345<span class="hljs-number">.67</span><br />
-                print(num2)<br />
-                <span class="hljs-comment"># 789012345.67</span>
-            </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {underScores}
+                </SyntaxHighlighter>
+            </div>
 
             <h2 id="binary-in-python">Binary in Python</h2>
 
             <p>You can write an integer in Python using binary syntax using the <code>0b</code> prefix:</p>
 
-            <pre class="hljs"><code><div>print(<span class="hljs-number">0b0001</span>)<br />
-                <span class="hljs-comment"># Prints 1</span><br /><br />
-
-                print(<span class="hljs-number">0b0101</span>)<br />
-                <span class="hljs-comment"># Prints 5</span>
-            </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {binaryInPython}
+                </SyntaxHighlighter>
+            </div>
 
             <p><em>Note</em> leading <code>0</code>s are often added for visual consistency but do not change the value of a binary number.</p>
 
@@ -46,21 +87,18 @@ export default function Computing() {
 
             <p>Similar to logical operators, but instead of operating on boolean values, bitwise operators apply the same logic to all the bits in a value by column. Example, the numbers 5 and 7 represented in binary would result in 5.</p>
 
-            <div class="center">
-                <pre class="hljs"><code><div>  0101<br />
-                    &amp; 0111<br />
-                    ------<br />
-                    &nbsp;&nbsp;0101<br />
-                </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="bash" style={tomorrow} className="center">
+                    {bitwiseAndOperator}
+                </SyntaxHighlighter>
             </div>
 
             <p>A $ 1 $ in binary is the same as <code>True</code>, while $ 0 $ is <code>False</code>. So really a bitwise operation is just a bunch of logical operations that are completed in tandem by column.</p>
 
-            <div class="center">
-                <pre class="hljs"><code><div>0 &amp; 0 = 0<br />
-                    1 &amp; 1 = 1<br />
-                    1 &amp; 0 = 0<br />
-                </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="bash" style={tomorrow} className="center">
+                    {andOperator}
+                </SyntaxHighlighter>
             </div>
 
             <p>The binary <code>&amp;</code> (ampersand) will always produce the same or less amount of $1 $s. It is also referred to as masking or filtering.</p>
@@ -73,23 +111,19 @@ export default function Computing() {
 
             <p>Remember that when writing a number in binary, the prefix <code>0b</code> is used to indicate that what follows is a binary number. <code>0b10</code> is two in binary, but <code>10</code> without the <code>0b</code> prefix is simply ten.</p>
 
-            <pre class="hljs"><code><div><span class="hljs-number">0b0101</span> &amp; <span class="hljs-number">0b0111</span><br />
-                <span class="hljs-comment"># equals 5</span><br /><br />
-
-                binary_five = <span class="hljs-number">0b0101</span><br />
-                binary_seven = <span class="hljs-number">0b0111</span><br />
-                binary_five &amp; binary_seven<br />
-                <span class="hljs-comment"># equals 5</span>
-            </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {binaryNotation}
+                </SyntaxHighlighter>
+            </div>
 
             <h2 id="convert-binary">Convert Binary</h2>
 
-            <pre class="hljs"><code><div><span class="hljs-comment"># this is a binary string</span><br />
-                binary_string = <span class="hljs-string">"100"</span><br /><br />
-
-                <span class="hljs-comment"># convert binary string to integer</span><br />
-                num = int(binary_string, <span class="hljs-number">2</span>)
-            </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {convertBinary}
+                </SyntaxHighlighter>
+            </div>
 
             <p>The first parameter in <code>int()</code> has to be a string of a binary number.</p>
 
