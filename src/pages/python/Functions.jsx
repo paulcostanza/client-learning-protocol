9282e68d697637b58a12fc7c719e273ef1c9eba7
paulcostanza <paulvincentcostanza@gmail.com>
2025-02-04 11:44:17 -0500

    updated python>functions page with react-syntax-highlighter

--- src/pages/python/Functions.jsx
+++ src/pages/python/Functions.jsx
@@ -1,4 +1,25 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 export default function Functions() {
+
+    const dynamicTyping = `speed = 7
+speed = "seven"`
+
+    const orderOfFunctions = `def main():
+    health = 10
+    armor = 5
+    add_armor(health, armor)
+
+def add_armor(h, a):
+    new_health = h + a
+    print_health(new_health)
+
+def print_health(new_health):
+    print(f"The player now has {new_health} health")
+
+# call entrypoint last
+main()`
     return (
         <>
             <h1>Functions</h1>
@@ -8,10 +29,11 @@ export default function Functions() {
             <p>Python is <em>dynamically typed</em> which means a variable can store any type, and that type can <em>change</em>.</p>
             <p>For example, if we make a number a variable, we can later change that variable to a string:</p>
 
-            {/* Code */}
-            <pre className="hljs"><code><div>speed = <span className="hljs-number">7</span><br />
-                speed = <span className="hljs-string">"seven"</span>
-            </div></code></pre>
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {dynamicTyping}
+                </SyntaxHighlighter>
+            </div>
 
             <p>In most circumstances, it is bad to change the type of a variable. The proper thing to do is to just create a new variable with the different data type.</p>
 
@@ -27,23 +49,11 @@ export default function Functions() {
 
             <p><em>Note</em> conventionally this <em>entry point</em> function is usually called <code>main</code> to keep things simple and consistent.</p>
 
-            {/* Code */}
-            <pre className="hljs"><code className="line-numbers"><div><span className="hljs-function"><span className="hljs-keyword">def</span> <span className="hljs-title">main</span><span className="hljs-params">()</span>:</span><br />
-                &nbsp;&nbsp;health = <span className="hljs-number">10</span><br />
-                &nbsp;&nbsp;armor = <span className="hljs-number">5</span><br />
-                &nbsp;&nbsp;add_armor(health, armor)<br /><br />
-
-                <span className="hljs-function"><span className="hljs-keyword">def</span> <span className="hljs-title">add_armor</span><span className="hljs-params">(h, a)</span>:</span><br />
-                &nbsp;&nbsp;new_health = h + a<br />
-                &nbsp;&nbsp;print_health(new_health)<br /><br />
-
-                <span className="hljs-function"><span className="hljs-keyword">def</span> <span className="hljs-title">print_health</span><span className="hljs-params">(new_health)</span>:</span><br />
-                &nbsp;&nbsp;print(<span className="hljs-string">f"The player now has <span className="hljs-subst">{"{new_health}"}</span> health"</span>)<br /><br />
-
-                <span className="hljs-comment"># call entrypoint last</span><br />
-                main()
-            </div></code></pre>
-
+            <div className="center">
+                <SyntaxHighlighter language="python" style={tomorrow} showLineNumbers className="center">
+                    {orderOfFunctions}
+                </SyntaxHighlighter>
+            </div>
 
             <h2 id="none-return">None Return</h2>
 
@@ -56,7 +66,7 @@ export default function Functions() {
             <p>So this means that arguments are the <em>actual values</em> that go into the funtion, like <code>69.0</code>, <code>True</code>, or <code>&quot;asian assassins&quot;</code>. Parameters are the names we use in the funtion definition to refer to those values (think of it as the placeholder).</p>
 
 
-            {/* accordion */}
+            {/* accordion question */}
             <div className="container accordion" id="this-question">
                 <div className="accordion-item">
                     <h2 className="accordion-header">
