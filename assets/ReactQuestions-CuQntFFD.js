6c3897348b18320b5e643241200b2116f1ddf95e
paulcostanza <68032869+paulcostanza@users.noreply.github.com>
2025-06-14 00:08:45 +0000

    Deploying to gh-pages from @ paulcostanza/client-learning-protocol@12c22c0ce3b67ef728c3884320033936616bbab8 🚀

---
+++ assets/ReactQuestions-CuQntFFD.js
@@ -0,0 +1,1 @@
+const a=[{id:1,question:"React is a _______ language",options:["Fun","Cool","Cool & Fun"],answer:"Cool & Fun"},{id:2,question:"React and JavaScript are...",options:["Easy","Medium","Hard"],answer:"Hard"}];export{a as default};
