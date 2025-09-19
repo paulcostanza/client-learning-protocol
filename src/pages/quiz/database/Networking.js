19380daee93587a7f1f274e747813c2c5dc3125f
paulcostanza <paulvincentcostanza@gmail.com>
2025-09-18 20:42:30 -0400

    added questions to Networking.js

--- src/pages/quiz/database/Networking.js
+++ src/pages/quiz/database/Networking.js
@@ -2,6 +2,8 @@ export default [
     {
         id: 1,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Layer Outlier',
         question: "Which device or service would not operate at the physical layer of the OSI Model",
         options: [
@@ -10,11 +12,14 @@ export default [
             'IP Address',
             'Cable',
         ],
+        random: true,
         answer: 'IP Address'
     },
     {
         id: 2,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Goodbye',
         question: "Which TCP flag is used to gracefully terminate a session between two systems",
         options: [
@@ -26,12 +31,15 @@ export default [
             'ACK',
             'RST'
         ],
+        random: true,
         answer: 'FIN'
     },
     {
         id: 3,
         type: 'radio',
-        title: 'What’s the Point?',
+        category: 'networking',
+        subcategory: '',
+        title: 'What\'s the Point?',
         question: "Which statement correctly characterizes the functions of the OSI Model",
         options: [
             'A method of describing the functions of a networking system to allow communication',
@@ -39,6 +47,7 @@ export default [
             'A protocol used for encrypting network traffic',
             'A type of network cable used in data centers'
         ],
+        random: true,
         answer: 'A method of describing the functions of a networking system to allow communication'
     },
     {
@@ -54,6 +63,7 @@ export default [
             'packet switching',
             'MAC addressing'
         ],
+        random: true,
         answer: [
             'encryption',
             'compression'
@@ -62,6 +72,8 @@ export default [
     {
         id: 6,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: '',
         question: "Which device or service would not operate at the physical layer of the OSI Model?",
         options: [
@@ -70,11 +82,14 @@ export default [
             'Switch',
             'Hub'
         ],
+        random: true,
         answer: 'IP Address'
     },
     {
         id: 7,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Communicate with who?',
         question: "Which type of communication would be analogous to a half duplex transmission",
         options: [
@@ -83,6 +98,7 @@ export default [
             'a one-way radio broadcast',
             'two people talking on the phone at the same time'
         ],
+        random: true,
         answer: 'two people using walkie-talkies'
     },
     {
@@ -98,6 +114,7 @@ export default [
             'broadcast',
             'multicast'
         ],
+        random: true,
         answer: [
             'connection-oriented',
             'TCP'
@@ -106,6 +123,8 @@ export default [
     {
         id: 10,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'I spy MTU size',
         question: "Which command can be used to determine the MTU optimal size?",
         options: [
@@ -117,6 +136,7 @@ export default [
             'nslookup',
             'ipconfig'
         ],
+        random: true,
         answer: 'ping'
     },
     {
@@ -134,6 +154,7 @@ export default [
             'ICMP',
             'PIMP'
         ],
+        random: true,
         answer: [
             'SMTP',
             'HTTP',
@@ -165,6 +186,7 @@ export default [
             'segmentation',
             'multiplexing'
         ],
+        random: true,
         answer: [
             'routing'
         ],
@@ -181,6 +203,8 @@ export default [
     {
         id: 13,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Who gets this?',
         question: "The TCP header specifies which value to ensure that data is processed by the correct application",
         options: [
@@ -193,11 +217,14 @@ export default [
             'Checksum',
             'TTL (Time To Live)'
         ],
+        random: true,
         answer: 'Port'
     },
     {
         id: 15,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Node-on-node action',
         question: "Which layer of the OSI model is responsible for node-to-node data delivery and framing",
         options: [
@@ -209,11 +236,14 @@ export default [
             'Presentation Layer',
             'Application Layer'
         ],
+        random: true,
         answer: 'Data Link Layer'
     },
     {
         id: 16,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Choose your weapon',
         question: "Which device or service operates at the Data Link Layer to manage node-to-node communication",
         options: [
@@ -225,11 +255,14 @@ export default [
             'bridge',
             'modem'
         ],
+        random: true,
         answer: 'switch'
     },
     {
         id: 17,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'LLC purpose',
         question: "What is the purpose of the Logical Link Control (LLC) sub-layer in the Data Link Layer",
         options: [
@@ -238,11 +271,14 @@ export default [
             'It is responsible for error detection and correction',
             'It converts data from bits into electrical signals'
         ],
+        random: true,
         answer: 'It manages communication with the network layer protocols'
     },
     {
         id: 18,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Physical layer magic',
         question: "What is a key distinction of the components at the physical layer of the OSI model",
         options: [
@@ -251,11 +287,14 @@ export default [
             'They define the routing paths for data across networks',
             'They are responsible for application protocols like HTTP'
         ],
+        random: true,
         answer: 'They involve encoding and signaling to transmit data'
     },
     {
         id: 19,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Not mine',
         question: `Which of the following is **NOT** a responsibility of the Data Link Layer`,
         options: [
@@ -264,12 +303,15 @@ export default [
             'Framing data for transmission',
             'Routing data between different networks'
         ],
+        random: true,
         answer: 'Routing data between different networks',
         description: `The Data Link Layer focuses on node-to-node communication within the same network — things like adding physical addresses, framing, handling error detection and retransmission.`
     },
     {
         id: 20,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: 'Bits to circuit',
         question: "In the OSI model, which layer is responsible for transforming data from bits into electrical signals for transmission",
         options: [
@@ -281,7 +323,277 @@ export default [
             'Presentation Layer',
             'Application Layer'
         ],
+        random: true,
         answer: 'Physical Layer',
+    },
+    {
+        id: 21,
+        type: 'checkbox',
+        title: '',
+        question: "What type of equipment resides at the Physical Layer of the OSI model?",
+        options: [
+            'router',
+            'hub',
+            'switch',
+            'network cabling',
+            'repeater'
+        ],
+        random: true,
+        answer: [
+            'hub',
+            'network cabling',
+            'repeater'
+        ]
+    },
+    {
+        id: 22,
+        type: 'checkbox',
+        title: '',
+        question: "Which of these protocols reside at the Transport Layer?",
+        options: [
+            'UDP',
+            'IP',
+            'HTTPS',
+            'ICMP',
+            'TCP',
+            'ARP'
+        ],
+        random: true,
+        answer: [
+            'UDP',
+            'TCP'
+        ]
+    },
+    {
+        id: 23,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "Which refer to a unique, 48-bit identifier also known as physical network address?",
+        options: [
+            'SSID',
+            'IP',
+            'GUID',
+            'MAC'
+        ],
+        random: true,
+        answer: 'MAC'
+    },
+    {
+        id: 24,
+        type: 'checkbox',
+        title: '',
+        question: "Which of the following reside(s) at the Data Link Layer?",
+        options: [
+            'bridge',
+            'media converter',
+            'switch',
+            'hub',
+            'network adapter'
+        ],
+        random: true,
+        answer: [
+            'bridge',
+            'switch',
+            'network adapter'
+        ]
+    },
+    {
+        id: 25,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "The protocol data unit at the Physical Layer is known as:",
+        options: [
+            'segment',
+            'frame',
+            'bit',
+            'packet',
+            'datagram'
+        ],
+        random: true,
+        answer: 'bit'
+    },
+    {
+        id: 26,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "Which layer assumes the responsibility for opening, closing, and maintaining connections between applications?",
+        options: [
+            'Physical Layer',
+            'Data Link Layer',
+            'Network Layer',
+            'Transport Layer',
+            'Session Layer',
+            'Presentation Layer',
+            'Application Layer'
+        ],
+        random: true,
+        answer: 'Session Layer',
+    },
+    {
+        id: 27,
+        type: 'checkbox',
+        title: '',
+        question: "Which of the protocols listed reside at the Network Layer of the OSI model?",
+        options: [
+            'ICMP',
+            'SIP',
+            'IP',
+            'ARP',
+            'SNMP'
+        ],
+        random: true,
+        answer: [
+            'ICMP',
+            'IP'
+        ]
+    },
+    {
+        id: 28,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "The Transport Layer's UDP protocol data unit is known as a:",
+        options: [
+            'bit',
+            'frame',
+            'packet',
+            'segment',
+            'datagram'
+        ],
+        random: true,
+        answer: 'datagram'
+    },
+    {
+        id: 29,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "Data format translation, data compression, and data encryption/decryption take place at which layer?",
+        options: [
+            'Physical Layer',
+            'Data Link Layer',
+            'Network Layer',
+            'Transport Layer',
+            'Session Layer',
+            'Presentation Layer',
+            'Application Layer'
+
+        ],
+        random: true,
+        answer: 'Presentation Layer',
+    },
+    {
+        id: 30,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "A NIC resides at which layer?",
+        options: [
+            'Physical Layer',
+            'Data Link Layer',
+            'Network Layer',
+            'Transport Layer',
+            'Session Layer',
+            'Presentation Layer',
+            'Application Layer'
+        ],
+        random: true,
+        answer: 'Data Link Layer',
+    },
+    {
+        id: 31,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "Which of the following devices resides at the network layer?",
+        options: [
+            'switch',
+            'repeater',
+            'bridge',
+            'router'
+        ],
+        random: true,
+        answer: 'router'
+    },
+    {
+        id: 32,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "The Data Link's protocol data unit is known as what?",
+        options: [
+            'bit',
+            'frame',
+            'packet',
+            'segment',
+            'datagram'
+        ],
+        random: false,
+        answer: 'frame'
+    },
+    {
+        id: 33,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "Which layer enables direct interaction between the end-user and the network?",
+        options: [
+            'Physical Layer',
+            'Data Link Layer',
+            'Network Layer',
+            'Transport Layer',
+            'Session Layer',
+            'Presentation Layer',
+            'Application Layer'
+
+        ],
+        random: true,
+        answer: 'Application Layer'
+    },
+    {
+        id: 34,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "The Network Layer protocol data unit is called what?",
+        options: [
+            'bit',
+            'frame',
+            'packet',
+            'segment',
+            'datagram'
+        ],
+        random: true,
+        answer: 'packet'
+    },
+    {
+        id: 35,
+        type: 'radio',
+        category: 'networking',
+        subcategory: '',
+        title: '',
+        question: "Which of the following answers refers to a common Data Link protocol?",
+        options: [
+            'HTTP',
+            'IP',
+            'ARP',
+            'ICMP'
+        ],
+        random: true,
+        answer: 'ARP'
     },
 
 ]
@@ -290,6 +602,8 @@ export default [
  {
         id: 1,
         type: 'radio',
+        category: 'networking',
+        subcategory: '',
         title: '', 'checkbox', or 'input' - need to create input though
         question: "Here is a question?",
         options: [
@@ -306,4 +620,52 @@ export default [
  * 
  * id: 5, 8, 14, & 21
  * 
+*/
+
+/*
+Here are some of the most common protocols associated with each OSI layer:
+
+Application (Layer 7):
+HTTP
+FTP
+SMTP
+POP3
+IMAP
+SNMP
+DNS
+SSH
+
+Presentation (Layer 6):
+SSL/TLS
+MPEG
+JPEG
+
+Session (Layer 5):
+NetBIOS
+RPC
+PPTP
+
+Transport (Layer 4):
+TCP
+UDP
+
+Network (Layer 3):
+IP
+ICMP
+ARP
+RIP
+OSPF
+
+Data Link (Layer 2):
+Ethernet
+PPP
+HDLC
+Frame Relay
+ATM
+
+Physical (Layer 1):
+Ethernet (physical signaling)
+USB
+Bluetooth
+RS-232
 */
