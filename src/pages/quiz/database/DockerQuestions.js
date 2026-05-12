691acd452d54590c1833807c8aa82b9ceb6288e4
paulcostanza <paulvincentcostanza@gmail.com>
2026-05-12 10:06:46 -0400

    fixed missing questions bug

---
+++ src/pages/quiz/database/DockerQuestions.js
@@ -0,0 +1,19 @@
+export default [
+    // {
+    //     id: 1,
+    //     type: 'radio',
+    //     category: 'docker',
+    //     subcategory: 'intro',
+    //     title: 'Speed comparison',
+    //     question: `Go code generally runs _____ than interpreted languages and compiles _____ than other compiled languages.`,
+    //     options: [
+    //         'slower, slower',
+    //         'slower, faster',
+    //         'faster, slower',
+    //         'faster, faster'
+    //     ],
+    //     random: true,
+    //     answer: 'faster, faster',
+    //     description: 'Go runs faster than interpreted languages like Python because it compiles to machine code, and compiles faster than languages like C++ due to its simpler compilation model.'
+    // },
+]
