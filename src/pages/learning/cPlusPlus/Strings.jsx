a9ec0784611e92ac78053062ae2465c45d4566cf
paulcostanza <paulvincentcostanza@gmail.com>
2026-05-16 11:25:54 -0400

    updated c++ > strings

--- src/pages/learning/cPlusPlus/Strings.jsx
+++ src/pages/learning/cPlusPlus/Strings.jsx
@@ -7,16 +7,41 @@ export default function Strings() {
         cPlusPlus: () => import('../../../pages/quiz/database/CPlusPlusQuestions.js')
     }
 
+    const newline = `std::cout << "Hello
+    world!";    // Error: not allowed!`
+
+    const concat = `std::cout << "Hello "
+    "world!"; // output is: Hello world! on one line`
+
     return (
         <div className="container">
             <h1>Strings</h1>
 
-            {/* <div className="">
-                <SyntaxHighlighter language="c++" style={tomorrow}
+            <h2>Quoted text takes the amount of whitespace literally</h2>
+
+            <p>This: <code>std::cout &lt;&lt; "Hello world!";</code></p>
+
+            <p>Is different than: <code>std::cout &lt;&lt; "Hello        world!";</code></p>
+
+            Newlines are not allowed in quoted text:
+
+            <div className="">
+                <SyntaxHighlighter language="cpp" style={tomorrow}
                     wrapLines={true} className=" code-snippet">
-                    {something}
+                    {newline}
                 </SyntaxHighlighter>
-            </div> */}
+            </div>
+
+            <p>Quoted text separated by nothing but whitespace such as spaces, tabs, or newlines will be concatenated:</p>
+
+            <div className="">
+                <SyntaxHighlighter language="cpp" style={tomorrow}
+                    wrapLines={true} className=" code-snippet">
+                    {concat}
+                </SyntaxHighlighter>
+            </div>
+
+            <p>To enable a string to print on multiple lines, you must throw in our friend the <strong>newline character</strong> (<code>\n</code>).</p>
 
             <hr />
 
