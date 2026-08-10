559908ab96a0459fbc3fef16094bb2c14919746b
paulcostanza <paulvincentcostanza@gmail.com>
2026-08-10 09:40:33 -0400

    added isinstance() to learning > python > built in functions section

--- src/pages/learning/python/BuiltInFunctions.jsx
+++ src/pages/learning/python/BuiltInFunctions.jsx
@@ -7,6 +7,29 @@ export default function BuiltInFunctions() {
         python: () => import('../../../pages/quiz/database/PythonQuestions.js')
     }
 
+    const isinstance = `isinstance(object, classinfo)`
+
+    const isinstanceTypeChecking = `age = 69
+print(isinstance(age, int)) # returns True`
+
+    const isinstanceCheckMultipleTypes = `num = 6.9
+print(isinstance(num, (int, float))) # returns True`
+
+    const isinstanceClassCheck = `class Animal:
+    pass
+
+class Dog(Animal):
+    pass
+
+my_dog = Dog()
+
+print(isinstance(my_dog, Dog)) # returns True
+print(isinstance(my_dog, Animal)) # returns True, because it accounts for inheritance`
+
+    const isinstanceVsType = `# bool is techincally a subclass of int in python
+print(isinstance(True, int)) # returns True since subclass logic matches
+print(type(True) is int) # returns False since the exact type is bool`
+
     const zipExample = `a = [1, 2, 3]
 b = [4, 5, 6]
 
@@ -61,6 +84,63 @@ print(reversed_dict)`
             <h1>Built-In Functions</h1>
 
             <h2><code>isinstance()</code></h2>
+
+            <p><code>isinstance()</code> is a built-in function that checks if an object is an instance of a specified class or any of its subclasses. It returns <code>True</code> if it matches, and <code>False</code> if it does not.</p>
+
+            <div className="">
+                <SyntaxHighlighter language="python" style={tomorrow} className="code-snippet">
+                    {isinstance}
+                </SyntaxHighlighter>
+            </div>
+
+            <ul>
+                <li><code>object</code>: the variable or runtime data instance you want to test</li>
+                <li><code>classinfo</code>: a class, primitive type, or a tuple of multiple classes/types</li>
+            </ul>
+
+            <h3>Use cases</h3>
+
+            <p><strong>Basic type checking.</strong></p>
+
+            <div className="">
+                <SyntaxHighlighter language="python" style={tomorrow} className="code-snippet">
+                    {isinstanceTypeChecking}
+                </SyntaxHighlighter>
+            </div>
+
+            <p><strong>Checking multiple types or logic.</strong> By passing a tuple as the second argument, <code>isinstance()</code> checks if the object is an instance of <em>any</em> type inside that tuple. The tuple acts like an <em>or</em> statement.</p>
+
+            <div className="">
+                <SyntaxHighlighter language="python" style={tomorrow} className="code-snippet">
+                    {isinstanceCheckMultipleTypes}
+                </SyntaxHighlighter>
+            </div>
+
+            <p><strong>Inheritance and subclasses.</strong> <code>isinstance()</code> walks up the object's <em>method resolution order chain</em> (MRO). If the object belongs to a child class, it will automatically return <code>True</code> for the parent class as well.</p>
+
+            <div className="">
+                <SyntaxHighlighter language="python" style={tomorrow} className="code-snippet">
+                    {isinstanceClassCheck}
+                </SyntaxHighlighter>
+            </div>
+
+            <p><strong>Works different that type.</strong> It works different than the built-in function <code>type()</code> as <code>type()</code> is more stric, requireing exact matches, ignores subclasses, and does not support tuples.</p>
+
+            <div className="">
+                <SyntaxHighlighter language="python" style={tomorrow} className="code-snippet">
+                    {isinstanceVsType}
+                </SyntaxHighlighter>
+            </div>
+
+            <h3>Under the hood</h3>
+
+            <p>At the interpreter layer, when you invoke <code>isinstance(obj, cls)</code>, python performs the following lookups:</p>
+
+            <ol>
+                <li>It looks to see if <code>cls</code> overrides behavior via a special <code>instancecheck</code> magic method.</li>
+                <li>If absent, the CPython runtime checks the underlying type of the object (<code>obj.class</code>).</li>
+                <li>It loops through the class's <code>mro</code> (method resolution order) to find a matching memory address representation of the queried class.</li>
+            </ol>
 
             <h2><code>zip()</code></h2>
 
